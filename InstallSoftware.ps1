e091d27bdbc7064ce144bc5f877efaf91537fe07
Alexander Neumann (EV-241) <Alexander.Neumann@bmw.de>
2021-04-12 09:50:17 +0200

    Added log entries

--- InstallSoftware.ps1
+++ InstallSoftware.ps1
@@ -1,8 +1,17 @@
 # https://devkimchi.com/2020/08/26/app-provisioning-on-azure-vm-with-chocolatey-for-live-streaming/
 
 
+# Logs
+mkdir "C:\\mylogs"
+echo "Starting script InstallSoftware.ps1 at $(Get-Date)" > "C:\\mylogs\install.txt"
+
 # Install Chocolatey
+echo "Starting to install Chocolatey at $(Get-Date)" > "C:\\mylogs\install.txt"
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+echo "Finished installing Chocolatey at $(Get-Date)" > "C:\\mylogs\install.txt"
+
 
 # Install Software
-# choco install vscode -y
+echo "Starting to install VSCode at $(Get-Date)" > "C:\\mylogs\install.txt"
+choco install vscode -y
+echo "Finished installing VSCode at $(Get-Date)" > "C:\\mylogs\install.txt"
